b3fd1b476fd1e3a0142941cec4d842d325fea4bb
kappppe <kasperlindberg91@gmail.com>
2023-12-12 13:53:23 +0100

    commit

---
+++ server/services/abuseipdbService.js
@@ -0,0 +1,25 @@
+
+async function abuseQuery() {
+
+const apiKey = process.env.abuseApiKey;
+  const baseURL = "https://api.abuseipdb.com/api/v2/check";
+  const ipAddress = "43.154.151.93";
+  const maxAgeInDays = "90";
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Key: apiKey,
+    },
+  };
+  const query = `${baseURL}?ipAddress=${ipAddress}&maxAgeInDays=${maxAgeInDays}`;
+
+  const response = await fetch(query, options);
+  const jsonBody = await response.json();
+  console.log(jsonBody);
+
+}
+
+module.exports = {
+    abuseQuery,
+  };
