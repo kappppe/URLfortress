6842488281b33e40a7b2fdfbc75f0a5c9e9d0453
itsNiazi <niazitareqmejl@gmail.com>
2024-01-06 14:27:59 +0100

    maybe works now

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import React, { useState } from "react";
+import SimpleMap from "./MapContainer";
 
 function Searchbar() {
   const [query, setQuery] = useState("");
@@ -37,17 +38,81 @@ function Searchbar() {
         Search
       </button>
 
-      {Object.keys(responseData).length > 0 && (
+      {Object.keys(responseData).length > 0 ? (
         <div>
           <h2>Host Io Result</h2>
           <p>Rank: {responseData.hostIoResult.rank}</p>
           <p>Facebook: {responseData.hostIoResult.facebook}</p>
           <p>Twitter: {responseData.hostIoResult.twitter}</p>
           <p>score: {responseData.abuseResult.score}</p>
+
+          {/* Conditionally render the map */}
+          {responseData.ipApiResult ? (
+            <SimpleMap center={responseData.ipApiResult} />
+          ) : (
+            <p>Loading map...</p>
+          )}
         </div>
+      ) : (
+        <p>No data available. Please perform a search.</p>
       )}
     </>
   );
 }
 
 export default Searchbar;
+
+// -----------------------------------------------------------
+// import { useState } from "react";
+
+// function Searchbar() {
+//   const [query, setQuery] = useState("");
+//   const [responseData, setResponseData] = useState({});
+
+//   const handleChange = (event) => {
+//     setQuery(event.target.value);
+//   };
+
+//   const baseURL = "http://localhost:3000/api/v1/fullreport/?query=";
+//   const queryString = `${baseURL}${query}`;
+
+//   const fetchData = async () => {
+//     try {
+//       const options = {
+//         method: "GET",
+//         headers: {
+//           Accept: "application/json",
+//         },
+//       };
+
+//       const results = await fetch(queryString, options);
+//       const jsonBody = await results.json();
+//       console.log(jsonBody);
+//       setResponseData(jsonBody);
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <label>Please, input the ip/domain address:</label>
+//       <input onChange={handleChange}></input>
+//       <button type="button" onClick={fetchData}>
+//         Search
+//       </button>
+
+//       {Object.keys(responseData).length > 0 && (
+//         <div>
+//           <h2>Host Io Result</h2>
+//           <p>Rank: {responseData.hostIoResult.rank}</p>
+//           <p>Facebook: {responseData.hostIoResult.facebook}</p>
+//           <p>Twitter: {responseData.hostIoResult.twitter}</p>
+//           <p>score: {responseData.abuseResult.score}</p>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
+
+// export default Searchbar;
