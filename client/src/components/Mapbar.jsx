5bba0987812c90e8b4ebf7fcb6c9f49c737ca8ef
kappppe <kasperlindberg91@gmail.com>
2024-01-06 13:47:54 +0100

    maps fix

--- client/src/components/Mapbar.jsx
+++ client/src/components/Mapbar.jsx
@@ -6,7 +6,7 @@ function Mapbar() {
       // Initial map with a generic location
       const map = new window.google.maps.Map(document.getElementById("map"), {
         center: { lat: 55.6078, lng: 12.9982 },
-        zoom: 20,
+        zoom: 4,
       });
     }
 
@@ -14,7 +14,7 @@ function Mapbar() {
     if (!window.google || !window.google.maps) {
       // Load the Google Maps API script
       const script = document.createElement("script");
-      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBUJpYWOqCM9EwE6vjP4wReHNt6wqX7yps&callback=initMap`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCIfZP-bCxWS4x3Dz-TKbAb39sRPqHl8sk&callback=initMap`;
       script.async = true;
 
       // Set a global callback function for when the script is loaded
@@ -28,8 +28,8 @@ function Mapbar() {
         // Remove the global callback function
         delete window.initMap;
       };
-    } else {
-      // If the API is already loaded, call initMap directly
+    } else if (!window.initMap) {
+      // If the API is already loaded, but initMap is not defined, call initMap directly
       initMap();
     }
   }, []); // The empty dependency array ensures that the effect runs only once
@@ -39,39 +39,3 @@ function Mapbar() {
 
 export default Mapbar;
 
-// import React, { useEffect } from "react";
-
-// function Mapbar() {
-//   useEffect(() => {
-//     let map;
-
-//     function initMap() {
-//       // Initial map with a generic location
-//       map = new window.google.maps.Map(document.getElementById("map"), {
-//         center: { lat: 0, lng: 0 },
-//         zoom: 2,
-//       });
-//     }
-
-//     // Check if the Google Maps API script has already been added
-//     if (!window.google || !window.google.maps) {
-//       // Load the Google Maps API script
-//       const script = document.createElement("script");
-//       script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyBUJpYWOqCM9EwE6vjP4wReHNt6wqX7yps&callback=initMap`;
-//       script.async = true;
-//       document.body.appendChild(script);
-
-//       // Cleanup function to remove the script when the component is unmounted
-//       return () => {
-//         document.body.removeChild(script);
-//       };
-//     } else {
-//       // If the API is already loaded, call initMap directly
-//       initMap();
-//     }
-//   }, []); // The empty dependency array ensures that the effect runs only once
-
-//   return <div id="map" style={{ height: "400px", width: "400px" }}></div>;
-// }
-
-// export default Mapbar;
