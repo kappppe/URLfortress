810c5807ab46c094af6b474c9dd367f1d43105aa
kappppe <kasperlindberg91@gmail.com>
2024-01-06 14:59:16 +0100

    upd.

--- client/src/components/MapContainer.jsx
+++ client/src/components/MapContainer.jsx
@@ -40,24 +40,4 @@ const MapContainer = ({ center }) => {
 
 export default MapContainer;
 
-//   const handleApiLoaded = (map, maps) => {
-//     // Use map and maps objects
-//     // You can perform any additional logic here after the Google Maps API is loaded
-//   };
 
-//   return (
-//     <div style={{ height: "50vh", width: "40%" }}>
-//       <GoogleMapReact
-//         bootstrapURLKeys={{ key: "AIzaSyBUJpYWOqCM9EwE6vjP4wReHNt6wqX7yps" }}
-//         defaultCenter={defaultProps.center}
-//         defaultZoom={defaultProps.zoom}
-//         yesIWantToUseGoogleMapApiInternals
-//         onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
-//       >
-//         <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-//       </GoogleMapReact>
-//     </div>
-//   );
-// };
-
-// export default SimpleMap;
