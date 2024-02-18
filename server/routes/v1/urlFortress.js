f06c75f2e969e9db4d9d06321fed1f9d0527062e
itsNiazi <niazitareqmejl@gmail.com>
2024-02-18 14:27:54 +0100

    checkpoint

--- server/routes/v1/urlFortress.js
+++ server/routes/v1/urlFortress.js
@@ -2,9 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const fullReportController = require("../../controllers/fullReportController");
-const threatInfo = require("../../controllers/threatInfo");
 
 router.get("/fullreport", fullReportController.fetchFullReport);
-router.get("/threatinfo", threatInfo.getThreatScore);
 
 module.exports = router;
